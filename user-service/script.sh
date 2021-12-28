8d87eb0c43ad9774a1adff3a4175057a461287c8
JungwooSim <cscd053@gmail.com>
2021-12-28 23:58:44 +0900

    Service Discovery / User Service - 등록

---
+++ user-service/script.sh
@@ -0,0 +1,9 @@
+mvn spring-boot:run -Dspring-boot.run.jvmArguments='-Dserver.port=9003'
+
+# 빌드된 파일 삭제
+mvn clean
+
+# 빌드 후 잡파일 생성
+mvn compile pakcage
+
+java -jar -Dserver.port=9004 ./target/user-service-0.0.1-SNAPSHOT.jar
