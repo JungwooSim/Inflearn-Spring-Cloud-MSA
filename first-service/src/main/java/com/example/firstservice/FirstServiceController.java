346ebb7b6591405492de840d6cbfc35421e6b5e8
JungwooSim <cscd053@gmail.com>
2021-12-29 17:52:50 +0900

    API Gateway Service / Netflix Zuul - 프로젝트 생성 API Gateway Service / Netflix Zuul - Filter 적용 API Gateway Service / Spring Cloud Gateway 란? API Gateway Service / Spring Cloud Gateway - 프로젝트 생성 API Gateway Service / Spring Cloud Gateway - Filter 적용 API Gateway Service / Spring Cloud Gateway - Custom Filter 적용 API Gateway Service / Spring Cloud Gateway - Global Filter 적용 API Gateway Service / Spring Cloud Gateway - Logging Filter 적용 API Gateway Service / Spring Cloud Gateway - Load Balancer

---
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -0,0 +1,57 @@
+package com.example.firstservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Enumeration;
+
+@RestController
+@RequestMapping("/first-service")
+//@RequestMapping("/")
+@Slf4j
+public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the First service.";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service.";
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        Enumeration<String> headers = request.getHeaderNames();
+        Collections.list(headers).stream().forEach(name -> {
+            Enumeration<String> values = request.getHeaders(name);
+            Collections.list(values).stream().forEach(value -> System.out.println(name + "=" + value));
+        });
+
+        log.info("Server port={}", request.getServerPort());
+
+        log.info("spring.cloud.client.hostname={}", env.getProperty("spring.cloud.client.hostname"));
+        log.info("spring.cloud.client.ip-address={}", env.getProperty("spring.cloud.client.ip-address"));
+
+        return String.format("Hi, there. This is a message from First Service on PORT %s"
+                , env.getProperty("local.server.port"));
+    }
+}
