3c4c8d99efda0d3424bea7bd091e3394deedcf96
JungwooSim <cscd053@gmail.com>
2021-12-29 17:59:39 +0900

    API Gateway Service / Netflix Zuul - 프로젝트 생성 API Gateway Service / Netflix Zuul - Filter 적용 API Gateway Service / Spring Cloud Gateway 란? API Gateway Service / Spring Cloud Gateway - 프로젝트 생성 API Gateway Service / Spring Cloud Gateway - Filter 적용 API Gateway Service / Spring Cloud Gateway - Custom Filter 적용 API Gateway Service / Spring Cloud Gateway - Global Filter 적용 API Gateway Service / Spring Cloud Gateway - Logging Filter 적용 API Gateway Service / Spring Cloud Gateway - Load Balancer

---
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,48 @@
+package com.example.apigatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Global filter baseMessage : request id -> {}", config.getBaseMessage());
+
+            if (config.isPreLogger()) {
+                log.info("Global filter Start : request id -> {}", request.getId());
+            }
+
+            // Custom Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Global filter End : request id -> {}", request.getId());
+                }
+            }));
+        };
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+
+        // Put the configuration properties
+    }
+}
