2d54215187dd29eec7cc78a6342821b342ee14ad
JungwooSim <cscd053@gmail.com>
2022-01-03 16:54:46 +0900

    Configuration Service / User Microservice 에서 Spring Cloud Config 연동

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -34,7 +34,12 @@ public class UserController {
 
     @GetMapping("/health_check")
     public String status() {
-        return String.format("It's Working in User Service on PORT %s", env.getProperty("local.server.port"));
+        return String.format("It's Working in User Service"
+                + ", port(local.server.port) = ", env.getProperty("local.server.port")
+                + ", port(server.port) = ", env.getProperty("server.port")
+                + ", token secret  = ", env.getProperty("token.secret")
+                + ", token expiration time= ", env.getProperty("token.expiration_time")
+        );
     }
 
     @GetMapping("/welcome")
