8ac19bce5dec707866ee7e84737b03c583e69c13
JungwooSim <cscd053@gmail.com>
2022-01-05 22:08:40 +0900

    데이터 동기화를 위한 Apache Kafka의 활용 / Apache Kafka 설치, Apache Kafka 사용 - Producer/Consumer

--- order-service/src/main/java/com/example/orderservice/jpa/OrderEntity.java
+++ order-service/src/main/java/com/example/orderservice/jpa/OrderEntity.java
@@ -16,7 +16,7 @@ public class OrderEntity implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 120, unique = true)
+    @Column(nullable = false, length = 120)
     private String productId;
 
     @Column(nullable = false)
--- user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
+++ user-service/src/main/java/com/example/userservice/client/OrderServiceClient.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @FeignClient(name = "order-service")
 public interface OrderServiceClient {
-    @GetMapping("/order-service/{userId}/orders_ng")
+    @GetMapping("/order-service/{userId}/orders")
     List<ResponseOrder> getOrders(@PathVariable String userId);
 }
